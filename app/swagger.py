3e68e4094224066ff59217b956432f8cdf0ad52b
Yuliia Kopysova <julykopysova97@gmail.com>
2021-07-17 22:43:54 +0300

    Cleaning config swagger file

--- app/swagger.py
+++ app/swagger.py
@@ -1,25 +1,10 @@
 from flask_swagger_ui import get_swaggerui_blueprint
 
-# SWAGGER_URL= /api/docs
-# API_URL= /static
-#
-# swaggerui_blueprint = get_swaggerui_blueprint('/api/docs', '/static')
-SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI
+API_URL = '/static/swagger.json'  # Our API url
 
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    SWAGGER_URL,
     API_URL
-    # config={  # Swagger UI config overrides
-    #     'app_name': "Test application"
-    # },
-    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
-    #    'clientId': "your-client-id",
-    #    'clientSecret': "your-client-secret-if-required",
-    #    'realm': "your-realms",
-    #    'appName': "your-app-name",
-    #    'scopeSeparator': " ",
-    #    'additionalQueryStringParams': {'test': "hello"}
-    # }
 )
