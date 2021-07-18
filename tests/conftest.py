290f3051f985b721dcb98a01fed78437a4a6da59
Yuliia Kopysova <julykopysova97@gmail.com>
2021-07-18 14:37:01 +0300

    Set up pytest, tested models

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+
+from app.models import Director, User, Genre, FilmHasGenre, Film
+
+
+@pytest.fixture(scope="module")
+def new_director():
+    director = Director("John", "Smith")
+    return director
+
+
+@pytest.fixture(scope="module")
+def new_user():
+    user = User("kim_lee", "Kim", "Lee", "kimlee@gmail.com", "1234", False)
+    return user
+
+
+@pytest.fixture(scope="module")
+def new_genre():
+    genre = Genre("Comedy")
+    return genre
+
+
+@pytest.fixture(scope="module")
+def new_film_genre():
+    film_has_genre = FilmHasGenre(1, 1)
+    return film_has_genre
+
+
+@pytest.fixture(scope="module")
+def new_film():
+    film = Film("Time", "2020-07-11T20:45:04", "description", 8, "/src", 1, 1)
+    return film
